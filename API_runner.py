afcfc0ae5f7d3604d8a3ef605f6faeace3f26fa4
abk4abhishek <abk4abhishek@gmail.com>
2017-08-14 12:05:25 +0530

    Basic structure

--- API_runner.py
+++ API_runner.py
@@ -1,9 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__,static_url_path='/static')
 
 @app.route('/')
-def hello_world():
-   return 'Hello World'
+def home():
+    return render_template('homepage.html')
+
+@app.route('/tc/')
+def testcase():
+    return render_template('testcase.html')
+
+@app.route('/tc/<int:tc_id>')
+def testcaseone(tc_id):
+    return 'Hello, %d' % tc_id
+
+@app.route('/ts/')
+def testset():
+    return render_template('testset.html')
+
+@app.route('/ts/<int:ts_id>')
+def testsetone(ts_id):
+    return 'Hello, %d' % ts_id
+
+
 
 if __name__ == '__main__':
    app.run()
